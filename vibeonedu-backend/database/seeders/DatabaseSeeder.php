f8723e130cd8a1061bec10c0e213f280e89b86e4
Andrej Jovanovski <andrejjovanovski17@gmail.com>
2024-10-19 18:14:42 +0200

    fixed seeders

--- vibeonedu-backend/database/seeders/DatabaseSeeder.php
+++ vibeonedu-backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\UserEvaluation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -23,7 +24,8 @@ class DatabaseSeeder extends Seeder
             CourseSeeder::class,
             TopicSeeder::class,
             UserTopicSeeder::class,
-
+            UserBadgeSeeder::class,
+            UserEvaluationSeeder::class,
 
         ]);
 
