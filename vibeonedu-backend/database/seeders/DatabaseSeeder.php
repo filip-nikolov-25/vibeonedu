0e437731ab800372c0fa1e48f9879bc81a43b9b4
Andrej Cvetkovski <cvetkovskia10@gmail.com>
2024-10-19 12:09:03 +0200

    BadgeSeeder call

--- vibeonedu-backend/database/seeders/DatabaseSeeder.php
+++ vibeonedu-backend/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             FunFactSeeder::class,
+            BadgeSeeder::class,
         ]);
 
         User::factory()->create([
