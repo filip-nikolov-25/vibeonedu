9888b8bc2b12a1eb2f749072d74c09c4107b5007
Slobodan Mitkov <slobodanmitkov145@gmail.com>
2024-10-19 11:18:45 +0200

    Added ModuleSeeder and ModuleFactory

--- vibeonedu-backend/database/seeders/ModuleSeeder.php
+++ vibeonedu-backend/database/seeders/ModuleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Module;
 
 class ModulesSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class ModulesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Module::factory(20)->create();
     }
 }
