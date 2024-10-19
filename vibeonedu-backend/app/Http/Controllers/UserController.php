45024d7d94ae99ceb31d702a0ec3ab8c7189f491
Andrej Cvetkovski <cvetkovskia10@gmail.com>
2024-10-19 16:02:31 +0200

    APIs for registered users and for badges

--- vibeonedu-backend/app/Http/Controllers/UserController.php
+++ vibeonedu-backend/app/Http/Controllers/UserController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Badge;
-use App\Http\Requests\StoreBadgeRequest;
-use App\Http\Requests\UpdateBadgeRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
 
-class BadgeController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $registrations = User::count();
+        return response()->json(["registrations" => $registrations]);
     }
 
     /**
@@ -27,7 +27,7 @@ class BadgeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreBadgeRequest $request)
+    public function store(Request $request)
     {
         //
     }
@@ -35,21 +35,15 @@ class BadgeController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Badge $badge, $id)
-    {     
-         $badge = Badge::find($id);
-
-         if (!$badge) {
-             return response()->json(['error' => 'Badge not found'], 404);
-         }
- 
-         return response()->json($badge);
+    public function show(string $id)
+    {
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Badge $badge)
+    public function edit(string $id)
     {
         //
     }
@@ -57,7 +51,7 @@ class BadgeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateBadgeRequest $request, Badge $badge)
+    public function update(Request $request, string $id)
     {
         //
     }
@@ -65,7 +59,7 @@ class BadgeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Badge $badge)
+    public function destroy(string $id)
     {
         //
     }
