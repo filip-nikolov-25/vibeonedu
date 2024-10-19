b24fcfbf51cd6dc67103e6c19d413ac9eea3c159
ersan <adziovskie@gmail.com>
2024-10-19 11:36:28 +0200

    courses

--- vibeonedu-backend/database/seeders/ModuleSeeder.php
+++ vibeonedu-backend/database/seeders/ModuleSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Module;
 
-class ModulesSeeder extends Seeder
+class ModuleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Module::create(['name' => 'Programming Basics', 'description' => 'Introductory programming module.']);
+        Module::create(['name' => 'Web Development', 'description' => 'Web Development using modern frameworks.']);
+        Module::create(['name' => 'Database Design', 'description' => 'Database Design module.']);
     }
 }
