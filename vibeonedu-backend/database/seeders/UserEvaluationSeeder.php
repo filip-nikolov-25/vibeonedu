8d661eb18b4d10160e1093927ae202628a3acaf8
Slobodan Mitkov <slobodanmitkov145@gmail.com>
2024-10-19 13:05:46 +0200

    Added UserEvaluationSeeder and UserEvaluationFactory

--- vibeonedu-backend/database/seeders/UserEvaluationSeeder.php
+++ vibeonedu-backend/database/seeders/UserEvaluationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\user_evaluation;
 
 class UserEvaluationSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class UserEvaluationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        user_evaluation::factory(10)->create();
     }
 }
