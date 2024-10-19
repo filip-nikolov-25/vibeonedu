b780fb9b048821f4e8d3fbebab20eb52b33e9376
ersan <adziovskie@gmail.com>
2024-10-19 12:32:31 +0200

    CoursesApi

--- vibeonedu-backend/routes/api.php
+++ vibeonedu-backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/courses', [CourseController::class, 'index']);  // Show all courses
+Route::get('/courses/{course}', [CourseController::class, 'show']);  // Show a specific course by ID
