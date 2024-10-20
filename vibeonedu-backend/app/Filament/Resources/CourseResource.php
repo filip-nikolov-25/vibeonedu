26d477cd6181daf4c6d9a6be67365f0ee1d926d6
Andrej Jovanovski <andrejjovanovski17@gmail.com>
2024-10-20 11:37:04 +0200

    filament integration

---
+++ vibeonedu-backend/app/Filament/Resources/CourseResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CourseResource\Pages;
+use App\Models\Course;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class CourseResource extends Resource
+{
+    protected static ?string $model = Course::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Courses';
+    protected static ?string $navigationGroup = 'Education';
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCourses::route('/'),
+            'create' => Pages\CreateCourse::route('/create'),
+            'edit' => Pages\EditCourse::route('/{record}/edit'),
+        ];
+    }
+}
