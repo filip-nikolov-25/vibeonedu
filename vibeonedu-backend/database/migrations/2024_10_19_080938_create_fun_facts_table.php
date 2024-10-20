5954fe90b0ef6ee934b558bd4f5e65afeedf883c
Andrej Jovanovski <andrejjovanovski17@gmail.com>
2024-10-20 09:28:16 +0200

    fixed funfacts api

--- vibeonedu-backend/database/migrations/2024_10_19_080938_create_fun_facts_table.php
+++ vibeonedu-backend/database/migrations/2024_10_19_080938_create_fun_facts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('fun_facts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('author');
             $table->timestamps();
         });
     }
